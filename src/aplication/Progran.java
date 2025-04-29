f866ad3da0549fd7edab199e32c0a40477a62687
JoaoVictorMarcal <jmarcalbarbosa@gmail.com>
2025-04-29 11:46:45 -0300

    Criada a classe Seller

--- src/aplication/Progran.java
+++ src/aplication/Progran.java
@@ -1,13 +1,18 @@
 package aplication;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Progran {
 
 	public static void main(String[] args) {
 
 		Department department = new Department(1, "Frigorifico");
-		System.out.println(department);
+		Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000, department);
+		
+		System.out.println(seller);
 	}
 
 }
