95d63d65bd42945004669c21803219d22b13aa67
JoaoVictorMarcal <jmarcalbarbosa@gmail.com>
2025-04-29 11:12:40 -0300

    Department entity class

---
+++ src/aplication/Progran.java
@@ -0,0 +1,13 @@
+package aplication;
+
+import model.entities.Department;
+
+public class Progran {
+
+	public static void main(String[] args) {
+
+		Department department = new Department(1, "Frigorifico");
+		System.out.println(department);
+	}
+
+}
